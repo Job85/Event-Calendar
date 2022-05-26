eac12f9444aca64b66e458c20467be2ed4372e8d
Job85 <julianjernigan@ymail.com>
2022-05-26 17:50:05 -0400

    created component to select a date range

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import CalendarComp from './components/CalendarComp'
+import DateRangeComp from './components/DateRangeComp';
 
 function App() {
   const locales = {
@@ -15,6 +16,10 @@ function App() {
         <div>
           <h3>Choose a Date for Your Event</h3>
           <CalendarComp />
+        </div>
+        <div>
+          <h3>Choose a Date Range for Event</h3>
+          <DateRangeComp />
         </div>
       </main>
     </div>
