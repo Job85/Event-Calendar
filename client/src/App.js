ae341c84a751d8e06cb2ca3dadf5ef0d04933ff7
Job85 <julianjernigan@ymail.com>
2022-05-26 12:38:50 -0400

    made header smaller

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
 
 function App() {
+  // const [value, setValue] = useState(new Date());
+  // function onChange(nextValue) {
+  //   setValue(nextValue);
+  // }
+  const [today, activeStartDate] = useState(new Date(2022, 0, 1));
+  function onChange(nextDay) {
+    activeStartDate(nextDay)
+  }
   return (
     <div className="App">
       <header className="App-header">
 
       </header>
+      <main>
+        <Calendar onChange={onChange} today={today} />
+      </main>
     </div>
   );
 }
