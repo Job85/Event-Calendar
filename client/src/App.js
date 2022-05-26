146301be4d957c8cf7dd51aefd29a98c2d85b0b3
Job85 <julianjernigan@ymail.com>
2022-05-26 14:14:55 -0400

    installed react-datepicker & date-fns

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,19 @@ import './App.css';
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 
-function App() {
-  // const [value, setValue] = useState(new Date());
-  // function onChange(nextValue) {
-  //   setValue(nextValue);
-  // }
-  const [value, onChange] = useState(new Date(2022, 0, 1));
+import DatePicker from 'react-datepicker';
 
+function App() {
+  const locales = {
+    'en-US': require('date-fns/locale/en-US')
+  }
   return (
     <div className="App">
       <header className="App-header">
 
       </header>
       <main>
-        <Calendar onChange={onChange} value={value} />
+        <Calendar onChange={onChange} value={value} locale={locales} />
       </main>
     </div>
   );
